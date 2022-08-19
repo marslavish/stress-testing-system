2bd72adc1fbe2ab08379b79997510b017073c198
marslavish <uniqueye608@gmail.com>
2022-08-19 16:15:02 +0800

    feat: improvements

--- src/pages/StressTesting/index.tsx
+++ src/pages/StressTesting/index.tsx
@@ -4,11 +4,13 @@ import { useEffect, useState } from 'react';
 import styles from './index.less';
 import StressTestingTable from './components/StressTestingTable';
 import { request } from 'umi';
+import { pick } from 'lodash';
 
 const StressTesting = () => {
   const [tableData, setTableData] = useState([]);
   const [isDisabled, setIsDisabled] = useState(true);
   const [selectedRows, setSelectedRows] = useState([]);
+  const [isRestarting, setIsRestarting] = useState(false);
   // const [selectedOption, setSelectedOption] = useState('');
   // const [searchQuery, setSearchQuery] = useState('');
 
@@ -25,6 +27,8 @@ const StressTesting = () => {
   };
 
   useEffect(() => {
+    getMonitor();
+
     const monitorInterval = setInterval(() => {
       getMonitor();
     }, 60000);
@@ -33,6 +37,55 @@ const StressTesting = () => {
       clearInterval(monitorInterval);
     };
   }, []);
+
+  const addStressTest = (statusCode: 0 | 1) => {
+    request('/api/v1/server/miner/addStressTest', {
+      method: 'post',
+      data: { ...selectedRows[0], status: statusCode },
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')!).token}`,
+      },
+    })
+      .then(() => {
+        // setIsSaving(false);
+        // message.success('Save successful.');
+        console.log('addStressTest: success');
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const removeFromMonitor = () => {
+    request('/api/v1/removeFromMonitor', {
+      method: 'post',
+      data: { mac: selectedRows.map((record) => record.mac) },
+      headers: {
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')!).token}`,
+      },
+    })
+      .then(() => {
+        // setIsSaving(false);
+        // message.success('Save successful.');
+        console.log('removeFromMonitor: success');
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const addToMonitor = () => {
+    request('/api/v1/addToMonitor', {
+      method: 'post',
+      data: {
+        minerAddresses: selectedRows.map((row) => pick(row, ['mac', 'ip'])),
+      },
+    })
+      .then(() => {
+        setIsRestarting(false);
+      })
+      .catch(console.log);
+  };
+
+  useEffect(() => {
+    console.log(selectedRows);
+  }, [selectedRows]);
 
   const handleSelectChange = (value: string) => {
     console.log(`selected ${value}`);
@@ -47,15 +100,22 @@ const StressTesting = () => {
   };
 
   const handleCompleteClick = () => {
-    // ...
+    addStressTest(0);
+    removeFromMonitor();
+    getMonitor();
   };
 
-  const handleSaveClick = () => {
-    // ...
+  const handleRestartClick = () => {
+    setIsRestarting(true);
+    removeFromMonitor();
+    addToMonitor();
+    getMonitor();
   };
 
   const handleRepairClick = () => {
-    // ...
+    addStressTest(1);
+    removeFromMonitor();
+    getMonitor();
   };
 
   return (
@@ -88,7 +148,7 @@ const StressTesting = () => {
             <Button type="primary" onClick={handleCompleteClick} disabled={isDisabled}>
               Complete
             </Button>
-            <Button onClick={handleSaveClick} disabled={isDisabled}>
+            <Button onClick={handleRestartClick} disabled={isDisabled} loading={isRestarting}>
               Restart
             </Button>
             <Button danger onClick={handleRepairClick} disabled={isDisabled}>
